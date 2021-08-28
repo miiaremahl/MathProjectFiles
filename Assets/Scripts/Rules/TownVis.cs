1ed1fb5966fa7bad4c795e8da232f7b47b42c82c
miia.remahl <miia.remahl@gmail.com>
2021-08-28 19:15:24 +0300

    Code and asset files

---
+++ Assets/Scripts/Rules/TownVis.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SVS;
+using UnityEngine;
+using static Visualizer;
+
+public class TownVis : MonoBehaviour
+{
+    public LSystemGenerator lsystem;
+    List<Vector3> positions = new List<Vector3>();
+    public RoadHelper rh;
+
+    public StructureHelper structureHelper;
+
+    [SerializeField]
+    private int length = 8;
+    [SerializeField]
+    private float angle = 90;
+    private int initrl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initrl = length;
+        createTown();
+    }
+
+    // Update is called once per frame
+   public void createTown()
+    {
+        length = initrl;
+        rh.Reset();
+        structureHelper.Reset();
+        positions.Clear();
+        var sequence = lsystem.GenerateSentance();
+        VisualizeSeq(sequence);
+        
+    }
+
+    public int Length
+    {
+        get
+        {
+            if (length > 0) { return length; }
+            else
+            {
+                return 1; //possibly random
+                //Random r = new Random();
+                //return r.Next(1, 3);
+            }
+
+
+        }
+        set => length = value;
+    }
+
+    private void VisualizeSeq(string seq)
+    {
+        Stack<AgentParamaters> savePoints = new Stack<AgentParamaters>();
+        var curPos = Vector3.zero;
+        Vector3 direction = Vector3.forward;
+        Vector3 tempPosition = Vector3.zero;
+
+        positions.Add(curPos);
+
+        foreach (var letter in seq)
+        {
+            letters l = (letters)letter;
+            switch (l)
+            {
+
+                case letters.save:
+                    savePoints.Push(new AgentParamaters
+                    {
+                        position = curPos,
+                        direction = direction,
+                        length = length
+                    });
+
+                    break;
+
+                case letters.load:
+                    if (savePoints.Count != 0)
+                    {
+                        var age = savePoints.Pop();
+                        curPos = age.position;
+                        direction = age.direction;
+                        length = age.length;
+                    }
+                    break;
+                case letters.draw:
+                    tempPosition = curPos;
+                    curPos += direction * length;
+                    // put houses
+                    rh.PlaceStreet(tempPosition, Vector3Int.RoundToInt(direction), length);
+                    //Random r = new Random();
+                    //length -= r.Next(1,3); //2
+                    length -= 2;
+                    positions.Add(curPos);
+                    break;
+                case letters.right:
+                    direction = Quaternion.AngleAxis(angle, Vector3.up) * direction;
+                    break;
+
+                case letters.left:
+                    direction = Quaternion.AngleAxis(-angle, Vector3.up) * direction;
+                    break;
+                default:
+                    break;
+            }
+        }
+        rh.fixRoad();
+        structureHelper.PlaceStructuresAroundRoad(rh.GetRoadPos());
+        
+    }
+
+}
---
+++ Assets/move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public double i = 0.0;
+    public int rot = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = new Vector3(0, 0, 0);
+    }
+
+    Vector3 moveKitty(bool x, float step)
+    {
+
+        return x ? new Vector3(transform.position.x + step, transform.position.y, transform.position.z) : new Vector3(transform.position.x, transform.position.y , transform.position.z + step);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        transform.position += new Vector3(1 * inputX, 0, 1 * inputY) * Time.deltaTime;
+
+
+         if (Input.GetKey (KeyCode.Z))
+            transform.Rotate(new Vector3(0, -rot, 0) * Time.deltaTime);
+        else
+            if (Input.GetKey(KeyCode.X))
+            transform.Rotate(new Vector3(0, rot, 0) * Time.deltaTime);
+        //else
+        //    if (Input.GetKey (KeyCode.UpArrow))
+        //    transform.position += Vector3.up * Time.deltaTime; 
+        //else
+        //    if (Input.GetKey (KeyCode.DownArrow))
+        //    transform.position += Vector3.down * Time.deltaTime; 
+    }
+}
